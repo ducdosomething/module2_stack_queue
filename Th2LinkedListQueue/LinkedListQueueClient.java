7eb4fe6ad55a51f1c3e7f98c1bdc0f4935c2b23d
ducdosomething <duclehoang211201@gmail.com>
2024-03-18 10:01:20 +0700

    Upload th2 Linked List Queue

---
+++ Th2LinkedListQueue/LinkedListQueueClient.java
@@ -0,0 +1,15 @@
+package Th2LinkedListQueue;
+
+public class LinkedListQueueClient {
+    public static void main(String[] args) {
+        MyLinkedListQueue queue = new MyLinkedListQueue();
+        queue.enqueue(10);
+        queue.enqueue(20);
+        queue.enqueue(0);
+        queue.enqueue(0);
+        queue.enqueue(30);
+        queue.enqueue(40);
+        queue.enqueue(50);
+        System.out.println("Dequeue item is " + queue.dequeue().key);
+    }
+}
---
+++ Th2LinkedListQueue/Node.java
@@ -0,0 +1,11 @@
+package Th2LinkedListQueue;
+
+public class Node {
+    public int key;
+    public Node next;
+
+    public Node(int key) {
+        this.key = key;
+        this.next = null;
+    }
+}
